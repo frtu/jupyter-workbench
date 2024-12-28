7efa9dc24386340347f6a2a1c0de57f10e516978
Tu, Fred <frederic.tu@gmail.com>
2024-12-28 22:35:11 +0100

    chore: update script based on new docker compose cmd

--- scripts.bash
+++ scripts.bash
@@ -1,32 +1,32 @@
 echo "Type 'startsearch' to start jupyter-search"
 startsearch() {
-  echo "Make sure you have ** docker-compose ** installed !!"
-  (cd docker/jupyter-search && exec docker-compose up)
+  echo "Make sure you have ** docker compose ** installed !!"
+  (cd docker/jupyter-search && exec docker compose up)
 }
 echo "Type 'startllm' to start jupyter-llm"
 startllm() {
-  echo "Make sure you have ** docker-compose ** installed !!"
-  (cd docker/jupyter-llm && exec docker-compose up)
+  echo "Make sure you have ** docker compose ** installed !!"
+  (cd docker/jupyter-llm && exec docker compose up)
 }
 echo "Type 'startpyspark' to start jupyter-pyspark"
 startpyspark() {
-  echo "Make sure you have ** docker-compose ** installed !!"
-  (cd docker/jupyter-pyspark && exec docker-compose up)
+  echo "Make sure you have ** docker compose ** installed !!"
+  (cd docker/jupyter-pyspark && exec docker compose up)
 }
 echo "Type 'startjspark' to start jupyter-sparkmagic"
 startjspark() {
-  echo "Make sure you have ** docker-compose ** installed !!"
-  (cd docker/jupyter-sparkmagic && exec docker-compose up)
+  echo "Make sure you have ** docker compose ** installed !!"
+  (cd docker/jupyter-sparkmagic && exec docker compose up)
 }
 echo "Type 'startjdatascience' to start jupyter-data-science-py"
 startjdatascience() {
-  echo "Make sure you have ** docker-compose ** installed !!"
-  (cd docker/jupyter-data-science-py && exec docker-compose up)
+  echo "Make sure you have ** docker compose ** installed !!"
+  (cd docker/jupyter-data-science-py && exec docker compose up)
 }
 echo "Type 'startjocr' to start jupyter-ocr-tesseact-py"
 startjocr() {
-  echo "Make sure you have ** docker-compose ** installed !!"
-  (cd docker/jupyter-ocr-tesseact-py && exec docker-compose up)
+  echo "Make sure you have ** docker compose ** installed !!"
+  (cd docker/jupyter-ocr-tesseact-py && exec docker compose up)
 }
 
 
@@ -73,6 +73,6 @@ rundocker() {
 
 echo "Type 'runcompose' to run"
 runcompose() {
-  echo "docker-compose up --force-recreate"
-  docker-compose up --force-recreate
+  echo "docker compose up --force-recreate"
+  docker compose up --force-recreate
 }
